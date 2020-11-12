c38a076f20cb203669a0786d7e7f3ff6e9293ee3
Anatoly <anatolyrytik@gmail.com>
2020-11-12 14:37:42 +0100

    initial commit

---
+++ src/main/java/com/bootjava/restvote/RestaurantVoteApplication.java
@@ -0,0 +1,13 @@
+package com.bootjava.restvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVoteApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVoteApplication.class, args);
+    }
+
+}
