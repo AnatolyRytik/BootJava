c2a89c40c96e74749598a16a1728e985664756ef
Anatoly <anatolyrytik@gmail.com>
2020-11-13 15:21:20 +0100

    2_01_data_jpa

--- src/test/java/ru/javaops/bootjava/RestaurantVotingApplicationTests.java
+++ src/test/java/ru/javaops/bootjava/RestaurantVotingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestaurantVoteApplicationTests {
+class RestaurantVotingApplicationTests {
 
     @Test
     void contextLoads() {
